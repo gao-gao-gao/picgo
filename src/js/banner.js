fe41a5871fc7062c09d5e63a6fc25cd17377cec4
gziqiong <gziqiong@linewell.com>
2021-02-05 16:50:02 +0800

    react

--- src/js/banner.js
+++ src/js/banner.js
@@ -24,7 +24,23 @@ function GetBannerImg(props) {
   const { imageUrl = "" } = props.list[props.index]
     ? props.list[props.index]
     : props.list;
-  return <img alt="" src={imageUrl} />;
+  const circle = props.list.map((item, index) => {
+    return (
+      <li
+        key={index}
+        onClick={(e) => props.change(e, index)}
+        style={{
+          backgroundPosition: props.index === index ? "-16px -343px" : "",
+        }}
+      ></li>
+    );
+  });
+  return (
+    <>
+      <img alt="" src={imageUrl} />
+      <ol className="circle">{circle}</ol>
+    </>
+  );
 }
 
 class Banner extends React.Component {
@@ -34,14 +50,41 @@ class Banner extends React.Component {
   }
 
   componentDidMount() {
-    // const time = setInterval(() => {
-    //   this.setState({ ImgIndex: (this.state.ImgIndex + 1) % 10 });
-    // }, 2000);
+    this.timer = setInterval(() => {
+      this.setState({ ImgIndex: (this.state.ImgIndex + 1) % 10 });
+    }, 2000);
+
     GetApi("banner").then((res) => {
       console.log(res.data.banners);
       this.setState({ ImgList: res.data.banners });
     });
   }
+
+  clear() {
+    clearInterval(this.timer);
+  }
+
+  start() {
+    this.timer = setInterval(() => {
+      this.setState({ ImgIndex: (this.state.ImgIndex + 1) % 10 });
+    }, 2000);
+  }
+
+  right() {
+    clearInterval(this.timer);
+    this.setState({ ImgIndex: (this.state.ImgIndex + 1) % 10 });
+  }
+
+  left() {
+    clearInterval(this.timer);
+    this.setState({ ImgIndex: (this.state.ImgIndex + 9) % 10 });
+  }
+
+  change = (e, index) => {
+    e.preventDefault();
+    console.log(index);
+    this.setState({ ImgIndex: index });
+  };
 
   render() {
     const { ImgList = [], ImgIndex = 0 } = this.state;
@@ -50,13 +93,24 @@ class Banner extends React.Component {
         <section className="banner">
           <GetBannerImgBg list={ImgList} index={ImgIndex}></GetBannerImgBg>
           <div className="carousel-main">
-            <div className="slider">
-              <GetBannerImg list={ImgList} index={ImgIndex}></GetBannerImg>
-              <ol className="circle"></ol>
+            <div
+              className="slider"
+              onMouseEnter={this.clear.bind(this)}
+              onMouseLeave={this.start.bind(this)}
+            >
+              <GetBannerImg
+                list={ImgList}
+                index={ImgIndex}
+                change={this.change}
+              ></GetBannerImg>
             </div>
             <div className="arrow">
-              <span className="arr arrow-left">&lt;</span>
-              <span className="arr arrow-right">&gt;</span>
+              <span className="arr arrow-left" onClick={this.left.bind(this)}>
+                &lt;
+              </span>
+              <span className="arr arrow-right" onClick={this.right.bind(this)}>
+                &gt;
+              </span>
             </div>
             <div className="upload">
               <a href="#!" className="change">
